681ddbfe04aae8facb0d4f0bba06d60fba5ce95b
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2006-02-28 03:09:12 +0000

    Use response event instead of button clicks. Set the filename and directory according to the file being edited.
    svn path=/trunk/; revision=48

--- gnome-subtitles/src/GnomeSubtitles/Application/Dialogs/SaveSubtitleDialog.cs
+++ gnome-subtitles/src/GnomeSubtitles/Application/Dialogs/SaveSubtitleDialog.cs
@@ -19,32 +19,45 @@
 
 using Gtk;
 using System;
+using System.IO;
 
 namespace GnomeSubtitles {
 
 public class SaveSubtitleDialog : GladeWidget {
 	private FileChooserDialog dialog = null;
 
-	public SaveSubtitleDialog(GUI gui) : base(gui,WidgetNames.SaveSubtitleDialog) {
-		dialog = (FileChooserDialog)GetWidget(WidgetNames.SaveSubtitleDialog);
+	public SaveSubtitleDialog(GUI gui) : base(gui, WidgetNames.SaveAsSubtitleDialog) {
+		dialog = (FileChooserDialog)GetWidget(WidgetNames.SaveAsSubtitleDialog);
 		dialog.TransientFor = gui.Window;
+		
+		if (gui.Core.Subtitles.Properties.IsFilePathRooted)
+			dialog.SetFilename(gui.Core.Subtitles.Properties.FilePath);
+		else {
+			dialog.SetCurrentFolder(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+			dialog.CurrentName = gui.Core.Subtitles.Properties.FileName;
+		}
 	}
 	
 
 	#pragma warning disable 169		//Disables warning about handlers not being used
 	
+	private void OnResponse (object o, ResponseArgs args) {
+		Console.WriteLine(args.ResponseId);
+		
+		if (args.ResponseId == ResponseType.Ok) {
+			//GUI.SaveAs(dialog.Filename, Subtitle);
+			Console.WriteLine("Saving As " + dialog.Filename);
+			CloseDialog(); 
+		}
+		else if (args.ResponseId == ResponseType.Cancel) {
+			CloseDialog();
+		}
+	}
+	
 	private void CloseDialog() {
 		dialog.Destroy();
 	}
 	
-    private void OnDelete (object o, DeleteEventArgs args) {
-    		CloseDialog();
-    }
-    
-    private void OnCancel (object o, EventArgs args) {
-    		CloseDialog();
-    }
-
 
 }
 
