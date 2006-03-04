c9e396da01a124e7147426bc01c01045e1c2bec6
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2006-03-04 18:48:50 +0000

    Moved to a separate file.
    svn path=/trunk/; revision=53

---
+++ gnome-subtitles/src/GnomeSubtitles/Application/WidgetNames.cs
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Gnome Subtitles, a subtitle editor for Gnome.
+ * Copyright (C) 2006 Pedro Castro
+ *
+ * Gnome Subtitles is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Gnome Subtitles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace GnomeSubtitles {
+
+public class WidgetNames {
+	public const string MainWindow = "mainWindow";
+	
+	/* File Menu */
+	public const string SaveMenuItem = "saveMenuItem";
+	public const string SaveAsMenuItem = "saveAsMenuItem";
+	
+	/* Edit Menu */
+	public const string UndoMenuItem = "undoMenuItem";
+	public const string RedoMenuItem = "redoMenuItem";
+	public const string CutMenuItem = "cutMenuItem";
+	public const string CopyMenuItem = "copyMenuItem";
+	public const string PasteMenuItem = "pasteMenuItem";
+	public const string ClearMenuItem = "clearMenuItem";
+	public const string PropertiesMenuItem = "propertiesMenuItem";
+	
+	public const string TimesMenuItem = "timesMenuItem";
+	public const string FramesMenuItem = "framesMenuItem";
+	
+	/* Toolbar */	
+	public const string SaveButton = "toolbuttonSave";
+	public const string UndoButton = "toolbuttonUndo";
+	public const string RedoButton = "toolbuttonRedo";
+	
+	/* Dialogs */
+	public const string AboutDialog = "aboutDialog";
+	public const string OpenDialog = "openDialog";
+	public const string SaveAsDialog = "saveAsDialog";
+	public const string SaveAsDialogFormatComboBox = "formatComboBox";
+	public const string SaveAsDialogEncodingComboBox = "encodingComboBox";
+	
+	/* Subtitle View */
+	public const string SubtitleView = "subtitleListView";
+	
+	/* Subtitle Edit */
+	public const string SubtitleEditHBox = "editAreaHBox";
+	public const string StartSpinButton = "startSpinButton";
+	public const string EndSpinButton = "endSpinButton";
+	public const string DurationSpinButton = "durationSpinButton";
+	public const string SubtitleTextView = "subtitleTextView";
+	
+}
+
+}
