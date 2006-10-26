a10e597e88015a5edb0a693abd127b88053e0253
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2006-10-26 15:38:31 +0000

    Added Url and Email handlers, to open the web browser or mail composition applications. Start showing the version of SubLib. Added short Comment on the application. Change name of application from the id GnomeSubtitles to the name "Gnome Subtitles"
    svn path=/trunk/; revision=271

--- gnome-subtitles/src/GnomeSubtitles/Application/Dialogs/AboutDialog.cs
+++ gnome-subtitles/src/GnomeSubtitles/Application/Dialogs/AboutDialog.cs
@@ -22,9 +22,34 @@ using System;
 namespace GnomeSubtitles {
 
 public class AboutDialog : GladeDialog {
+	private new Gtk.AboutDialog dialog = null;
 
-	public AboutDialog (GUI gui) : base(gui, WidgetNames.AboutDialog){
-		(dialog as Gtk.AboutDialog).Version = ExecutionInfo.Version;
+	public AboutDialog (GUI gui) : base(gui, WidgetNames.AboutDialog, PreWidgetCreation) {
+		dialog = base.dialog as Gtk.AboutDialog;
+		
+		SetInformation();
+	}
+	
+	/* Private members */
+	
+		
+	private void SetInformation () {
+		dialog.Name = "Gnome Subtitles";
+		dialog.Comments += "\n\nUsing SubLib " + ExecutionInfo.SubLibVersion;
+		dialog.Version = ExecutionInfo.Version;
+	}
+	
+	private static void PreWidgetCreation () {
+		Gtk.AboutDialog.SetUrlHook(AboutDialogOpenUrl);
+		Gtk.AboutDialog.SetEmailHook(AboutDialogOpenEmail);
+	}
+	
+	private static void AboutDialogOpenUrl (Gtk.AboutDialog about, string url) {
+		Utility.OpenUrl(url);
+	}
+	
+	private static void AboutDialogOpenEmail (Gtk.AboutDialog about, string email) {
+		Utility.OpenSendEmail(email);
 	}
 
 }
