e191a5583ae39246ff645a53c1917e9c2042e16f
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2006-10-28 21:30:28 +0000

    Detect when the combobox is changed and update the filename accordingly. If the filename contains a subtitle extension, it's replaced with the extension of the selected format (in the combobox). If the filename doesn't contain an extension, it is added. When saving, the extension is replaced only if the current extension isn't in the list of extensions for the selected subtitle format.
    svn path=/trunk/; revision=278

--- gnome-subtitles/src/GnomeSubtitles/Application/Dialogs/SaveAsDialog.cs
+++ gnome-subtitles/src/GnomeSubtitles/Application/Dialogs/SaveAsDialog.cs
@@ -30,52 +30,109 @@ public class SaveAsDialog : SubtitleFileChooserDialog {
 	private ComboBox encodingComboBox = null;
 	private SubtitleTypeInfo[] subtitleTypes = null;
 
-	public SaveAsDialog(GUI gui) : base(gui, WidgetNames.SaveAsDialog) {
+	public SaveAsDialog (GUI gui) : base(gui, WidgetNames.SaveAsDialog) {
 		dialog.DoOverwriteConfirmation = true;
-	
+		
+		if (gui.Core.Subtitles.Properties.IsFilePathRooted)
+			dialog.SetCurrentFolder(gui.Core.Subtitles.Properties.FileDirectory);
+		else
+			dialog.SetCurrentFolder(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+		
+		dialog.CurrentName = gui.Core.Subtitles.Properties.FileName;
 		formatComboBox = (ComboBox)GetWidget(WidgetNames.SaveAsDialogFormatComboBox);
 		encodingComboBox = (ComboBox)GetWidget(WidgetNames.SaveAsDialogEncodingComboBox);
-		
 		FillFormatComboBox();
 		FillEncodingComboBox(encodingComboBox);
-		
-		if (gui.Core.Subtitles.Properties.IsFilePathRooted) {
-			System.Console.WriteLine("File path is: ");
-			System.Console.WriteLine(gui.Core.Subtitles.Properties.FilePath);
-			dialog.SetFilename(gui.Core.Subtitles.Properties.FilePath);
-		}
-		else {
-			dialog.SetCurrentFolder(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
-			dialog.CurrentName = gui.Core.Subtitles.Properties.FileName;
-		}
 	}
-	
+
 	private void FillFormatComboBox () {
-		SubtitleTypeInfo[] types = Subtitles.AvailableTypesSorted;
-		SubtitleType currentType = GUI.Core.Subtitles.Properties.SubtitleType;
-		int activeFormat = 0, typeNumber = 0;
+		subtitleTypes = Subtitles.AvailableTypesSorted;
+		SubtitleType subtitleType = GUI.Core.Subtitles.Properties.SubtitleType; //The type of the subtitle file
+		int activeType = -1; //The position of the combobox to make active
+		int microDVDTypeNumber = -1; //The position of the MicroDVD format in the combobox
+		int subRipTypeNumber = -1; //The position of the SubRip format in the combobox
 		
-		foreach (SubtitleTypeInfo typeInfo in types) {
-			if (typeInfo.Type == currentType)
-				activeFormat = typeNumber;
+		int currentTypeNumber = 0;
+		foreach (SubtitleTypeInfo typeInfo in subtitleTypes) {
+			SubtitleType type = typeInfo.Type;
+			
+			if (type == subtitleType)
+				activeType = currentTypeNumber;
+			
+			if (type == SubtitleType.MicroDVD)
+				microDVDTypeNumber = currentTypeNumber;
+			else if (type == SubtitleType.SubRip)
+				subRipTypeNumber = currentTypeNumber;
 				
 			formatComboBox.AppendText(typeInfo.Name + " (" + typeInfo.ExtensionsAsText + ")");
-			typeNumber++;
+			currentTypeNumber++;
 		}
-		
-		formatComboBox.Active = activeFormat;
-		subtitleTypes = types;
+
+		if (subtitleType == SubtitleType.Unknown) { //Active type isn't known, selecting MicroDVD or SubRip depending on Timing Mode
+			TimingMode timingMode = GUI.Core.TimingMode;
+			if ((timingMode == TimingMode.Frames) && (microDVDTypeNumber != -1))
+				formatComboBox.Active = microDVDTypeNumber;
+			else if ((timingMode == TimingMode.Times) && (subRipTypeNumber != -1))
+				formatComboBox.Active = subRipTypeNumber;
+			else
+				formatComboBox.Active = 0;
+		}
+		else {
+			formatComboBox.Active = activeType;
+		}
 	}
 	
-	private string HandleFileNameWithExtension (SubtitleType type) {
+	private string UpdateFileNameExtension (string fileName, SubtitleType type) {
 		SubtitleTypeInfo typeInfo = Subtitles.GetAvailableType(type);
-		string extension = typeInfo.PreferredExtension;
-		string fileName = dialog.Filename;
+		string newExtensionDotted = "." + typeInfo.PreferredExtension;
+
+		int index = -1;
+		string extensionDotted = "." + GetFileNameExtension(fileName, out index);
 		
-		if (fileName.EndsWith(extension))
+		if (extensionDotted == newExtensionDotted) //filename already has the correct extension
+			return fileName;
+		else if (index == -1) //filename doesn't have an extension, appending
+			return fileName + newExtensionDotted;
+		else if (IsSubtitleExtension(extensionDotted))  { //filename's extension is a subtitle extension
+			int dotIndex = index - 1;
+			return fileName.Substring(0, dotIndex) + newExtensionDotted;
+		}
+		else //filename's extension is not a subtitle extension
+			return fileName + newExtensionDotted;
+	}
+
+	private string AddExtensionIfNeeded (SubtitleType type) {
+		string fileName = dialog.Filename;
+		int index = 0;
+		string extension = GetFileNameExtension(fileName, out index);
+
+		SubtitleTypeInfo typeInfo = Subtitles.GetAvailableType(type);
+		if (typeInfo.HasExtension(extension))
 			return fileName;
 		else
-			return fileName + "." + extension;
+			return fileName + "." + typeInfo.PreferredExtension;
+	}
+	
+	private bool IsSubtitleExtension (string dottedExtension) {
+		string extension = dottedExtension.Substring(1); //Remove the starting dot
+		foreach (SubtitleTypeInfo type in subtitleTypes) {
+			if (type.HasExtension(extension))
+				return true;
+		}
+		return false;
+	}
+
+	/// <summary>Returns the extension for the specified filename.</summary>
+	private string GetFileNameExtension (string fileName, out int index) {
+		int dotIndex = fileName.LastIndexOf('.');
+		if ((dotIndex != -1) && (dotIndex != (fileName.Length - 1))) {
+			index = dotIndex + 1;
+			return fileName.Substring(index);
+		}
+		else {
+			index = -1;
+			return String.Empty;
+		}
 	}
 
 
@@ -85,13 +142,24 @@ public class SaveAsDialog : SubtitleFileChooserDialog {
 		if (args.ResponseId == ResponseType.Ok) {
 			SubtitleType type = subtitleTypes[formatComboBox.Active].Type;
 			Encoding encoding = Encoding.GetEncoding(encodings[encodingComboBox.Active].CodePage);
-			string fileName = HandleFileNameWithExtension(type);
+			string fileName = AddExtensionIfNeeded(type);
 			
 			GUI.SaveAs(fileName, type, encoding);
 			actionDone = true;
 		}
 		
 		CloseDialog();
+	}
+	
+	private void OnFormatChanged (object o, EventArgs args) {
+		SubtitleType type = subtitleTypes[formatComboBox.Active].Type;
+		string fileName = dialog.Filename;
+		string folder = dialog.CurrentFolder;
+		if (folder.Length != 0) {
+			fileName = fileName.Substring(folder.Length + 1);
+		}
+		fileName = UpdateFileNameExtension(fileName, type);
+		dialog.CurrentName = fileName;
 	}
 
 }
