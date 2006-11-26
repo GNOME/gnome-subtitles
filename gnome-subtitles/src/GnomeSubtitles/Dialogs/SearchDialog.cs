c6e7cfa0d188e81658ff19d764e3344a72a87b7e
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2006-11-26 16:10:54 +0000

    The search dialog now includes most of the Find functionality. Replace functions aren't implemented yet.
    svn path=/trunk/; revision=367

--- gnome-subtitles/src/GnomeSubtitles/Dialogs/SearchDialog.cs
+++ gnome-subtitles/src/GnomeSubtitles/Dialogs/SearchDialog.cs
@@ -20,107 +20,163 @@
 using Glade;
 using Gtk;
 using System;
-using SubLib;
+using System.Text.RegularExpressions;
 
 namespace GnomeSubtitles {
 
 internal enum SearchDialogResponse { Find = 1, Replace, ReplaceAll, Close };
 
+//TODO validate entry when using a regular expression (seems to be working as it is, though)
+//TODO check cases when dialog is opened and a change from find to replace is required
 public class SearchDialog : GladeDialog {
+	private string text = String.Empty;	//The text to search for
+	private Regex regex = null; 		//The regex that corresponds to the text and the options
+	private bool valuesCanDiffer = false; //Whether the values of the dialog might have been changed since the last Find
 
-	/* TODO: are these really needed? */
-	private Subtitles subtitles = null;
-	private SubtitleView subtitleView = null;
-	private SubtitleEdit subtitleEdit = null;
-	
+	private bool matchCase = false;
+	private bool backwards = false;
+	private bool useRegex = false;
+	private bool wrap = true;
+
 	/* Constant strings */
 	private const string dialogName = "searchDialog";
 
 	/* Widgets */
 	
-	[WidgetAttribute]
-	private Entry searchForEntry;
-	/*[WidgetAttribute]
-	private Entry replaceWithEntry;*/
-	[WidgetAttribute]
-	private CheckButton matchCaseCheckButton;	
-	/*[WidgetAttribute]
-	private CheckButton searchBackwardsCheckButton;*/
-	[WidgetAttribute]
-	private CheckButton wrapAroundCheckButton;
+	[WidgetAttribute] private Entry findEntry;
+	[WidgetAttribute] private Entry replaceEntry;
+	[WidgetAttribute] private Label replaceLabel;
+	[WidgetAttribute] private Table table;
+	
+	[WidgetAttribute] private CheckButton matchCaseCheckButton;	
+	[WidgetAttribute] private CheckButton backwardsCheckButton;
+	[WidgetAttribute] private CheckButton regexCheckButton;
+	[WidgetAttribute] private CheckButton wrapCheckButton;
+	
+	[WidgetAttribute] private Button buttonClose;
+	[WidgetAttribute] private Button buttonReplaceAll;
+	[WidgetAttribute] private Button buttonReplace;
+	[WidgetAttribute] private Button buttonFind;
 
-	public SearchDialog (bool showReplace) : base(dialogName) {
-		 subtitles = Global.Subtitles;
-		 subtitleView = Global.GUI.View;
-		 subtitleEdit = Global.GUI.Edit;
-		 
-		 SetTextToFind();
+	public SearchDialog () : base(dialogName) {
 	}
 	
-	//if text is selected, uses it. if not, uses history.
-	private void SetTextToFind () {
-		string selection = subtitleEdit.TextSelection;
-		if (selection != String.Empty) {
-			TextToFind = selection;				
+	public bool ShowReplace {
+		set {
+			if (value == true) {
+				dialog.Title = "Replace";
+				table.RowSpacing = 12;
+			}
+			else {
+				dialog.Title = "Find";
+				table.RowSpacing = 0;
+			}
+		
+			replaceEntry.Visible = value;
+			replaceLabel.Visible = value;
+		
+			buttonReplaceAll.Visible = value;
+			buttonReplace.Visible = value;
 		}
-		SelectSearchForText();
 	}
 	
-	private bool MatchCase {
+	public Regex Regex {
+		get { return regex; }
+	}
+	
+	public bool MatchCase {
 		get { return matchCaseCheckButton.Active; }
 	}
-	/*
-	private bool SearchBackwards {
-		get { return searchBackwardsCheckButton.Active; }
-	}*/
-	
-	private bool WrapAround {
-		get { return wrapAroundCheckButton.Active; }
+
+	public bool Backwards {
+		get { return backwardsCheckButton.Active; }
 	}
 	
-	private string TextToFind {
-		get { return searchForEntry.Text; }
-		set { searchForEntry.Text = value; }
-	}
-	/*
-	private string TextToReplace {
-		get { return replaceWithEntry.Text; }
-		set { replaceWithEntry.Text = value; }
-	}*/
-	
-	private void SelectSearchForText () {
-		searchForEntry.SelectRegion(0, TextToFind.Length);
+	public bool UseRegex {
+		get { return regexCheckButton.Active; }
 	}
 	
-	private void GetPositions (out int subtitle, out int index) {
-		subtitle = 0;
-		index = 0;
+	public bool Wrap {
+		get { return wrapCheckButton.Active; }
+	}
+	
+	public override void ShowDialog() { //TODO add ShowReplace here?
+		LoadDialogValues();
+		base.ShowDialog();
+	}
+	
+	
+	/* Private properties */
 		
-		if (subtitleView.Selection.Count > 0) {
-			subtitle = Util.PathToInt(subtitleView.Selection.FirstPath);
-			if (subtitleEdit.TextIsFocus)
-				index = subtitleEdit.TextCursorIndex;
+	private bool ValuesHaveChanged {
+		get {
+			if (text != findEntry.Text)
+				return true;
+			if (matchCase != matchCaseCheckButton.Active)
+				return true;
+			if (backwards != backwardsCheckButton.Active)
+				return true;
+			if (useRegex != regexCheckButton.Active)
+				return true;
+			if (wrap != wrapCheckButton.Active)
+				return true;
+			
+			return false;
 		}
 	}
-
-	private void Find () {
-		int startSubtitle, startIndex;
-		GetPositions(out startSubtitle, out startIndex);
-		
-		int foundIndex;
-		int foundSubtitle = subtitles.Find(TextToFind, startSubtitle, startIndex, WrapAround,
-			MatchCase, out foundIndex);
-		
-		System.Console.WriteLine("found: " + foundSubtitle + " " + foundIndex);
-
-		/*if (!SearchBackwards) {
-			subtitles.Find(TextToFind, 
-			subtitles.find
-		}*/
 	
+	/* Private methods */
+	
+	private void LoadDialogValues () {
+		SetFindEntryText();
+		matchCaseCheckButton.Active = matchCase;
+		backwardsCheckButton.Active = backwards;
+		regexCheckButton.Active = useRegex;
+		wrapCheckButton.Active = wrap;
 	}
 	
-	/* Private members */
+	/// <summary>Sets the text in the Find entry.</summary>
+	/// <remarks>Updating the text will select it and grab the focus to the entry.</remarks>
+	private void SetFindEntryText () {
+		string currentSelection = Global.GUI.Edit.TextSelection;
+		string textToUse = (currentSelection != String.Empty ? currentSelection : text);
+		findEntry.Text = textToUse;
+		findEntry.SelectRegion(0, textToUse.Length);
+		findEntry.GrabFocus();
+	}
+
+	private void SaveDialogValues () {
+		text = findEntry.Text;
+		matchCase = matchCaseCheckButton.Active;
+		backwards = backwardsCheckButton.Active;
+		useRegex = regexCheckButton.Active;
+		wrap = wrapCheckButton.Active;	
+	}
+	
+	private void Find () {
+		bool updateRegex = (valuesCanDiffer && ValuesHaveChanged);
+		SaveDialogValues();
+		if (updateRegex)
+			UpdateRegex();
+
+		if (backwards)
+			Global.GUI.View.Search.FindPrevious();
+		else
+			Global.GUI.View.Search.FindNext();
+	}
+	
+	private void UpdateRegex() {
+		RegexOptions options = RegexOptions.Singleline;
+		if (!matchCase)
+			options |= RegexOptions.IgnoreCase;
+		if (backwards)
+			options |= RegexOptions.RightToLeft;
+		
+		string regexText = (useRegex ? text : Regex.Escape(text));
+		regex = new Regex(regexText, options);		
+	}
+
+	/* Event members */
 	
 	#pragma warning disable 169		//Disables warning about handlers not being used
 	
@@ -128,7 +184,6 @@ public class SearchDialog : GladeDialog {
 		SearchDialogResponse response = (SearchDialogResponse)args.ResponseId;
 		switch (response) {
 			case SearchDialogResponse.Find:
-				System.Console.WriteLine("Find");
 				Find();
 				break;
 			case SearchDialogResponse.Replace:
@@ -139,10 +194,38 @@ public class SearchDialog : GladeDialog {
 				break;
 			case SearchDialogResponse.Close:
 				System.Console.WriteLine("Close");
-				CloseDialog();
+				HideDialog();
 				break;
 		}
 	}
+
+	private void OnFindTextChanged (object o, EventArgs args) {
+		valuesCanDiffer = true;
+		if (findEntry.Text.Length == 0)
+			buttonFind.Sensitive = false;
+		else
+			buttonFind.Sensitive = true;
+	}
+	
+	private void OnReplaceTextChanged (object o, EventArgs args) {
+		valuesCanDiffer = true;
+	}
+	
+	private void OnMatchCaseToggled (object o, EventArgs args) {
+		valuesCanDiffer = true;
+	}
+	
+	private void OnBackwardsToggled (object o, EventArgs args) {
+		valuesCanDiffer = true;
+	}
+
+	private void OnUseRegexToggled (object o, EventArgs args) {
+		valuesCanDiffer = true;
+	}
+	
+	private void OnWrapToggled (object o, EventArgs args) {
+		valuesCanDiffer = true;
+	}	
 
 }
 
