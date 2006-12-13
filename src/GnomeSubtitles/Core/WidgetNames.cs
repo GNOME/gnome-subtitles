1dcf3266dfb18c30de70c0ba04582d2f3fa9a702
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2006-12-13 10:58:45 +0000

    Tagging the 0.1 release.
    svn path=/tags/gnome-subtitles-0.1/; revision=420

---
+++ src/GnomeSubtitles/Core/WidgetNames.cs
@@ -0,0 +1,91 @@
+/*
+ * This file is part of Gnome Subtitles, a subtitle editor for Gnome.
+ * Copyright (C) 2006 Pedro Castro
+ *
+ * Gnome Subtitles is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Gnome Subtitles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace GnomeSubtitles {
+
+//TODO this shouldn't need to exist
+public class WidgetNames {
+	public const string MainWindow = "mainWindow";
+	
+	/* File Menu */
+	public const string SaveMenuItem = "saveMenuItem";
+	public const string SaveAsMenuItem = "saveAsMenuItem";
+	public const string HeadersMenuItem = "headersMenuItem";
+	
+	/* Edit Menu */
+	public const string UndoMenuItem = "undoMenuItem";
+	public const string RedoMenuItem = "redoMenuItem";
+	public const string CutMenuItem = "cutMenuItem";
+	public const string CopyMenuItem = "copyMenuItem";
+	public const string PasteMenuItem = "pasteMenuItem";
+	public const string InsertSubtitleMenuItem = "insertSubtitleMenuItem";
+	public const string InsertSubtitleBeforeMenuItem = "insertSubtitleBeforeMenuItem";
+	public const string DeleteSubtitlesMenuItem = "deleteSubtitlesMenuItem";
+	
+	/* View Menu */
+	public const string TimesMenuItem = "timesMenuItem";
+	public const string FramesMenuItem = "framesMenuItem";
+	
+	/* Search Menu */
+	public const string FindMenuItem = "findMenuItem";
+	public const string FindNextMenuItem = "findNextMenuItem";
+	public const string FindPreviousMenuItem = "findPreviousMenuItem";
+	public const string ReplaceMenuItem = "replaceMenuItem";
+	
+	/* Format Menu */
+	public const string BoldMenuItem = "boldMenuItem";
+	public const string ItalicMenuItem = "italicMenuItem";
+	public const string UnderlineMenuItem = "underlineMenuItem";
+
+	/* Timings Menu */
+	public const string InputFrameRateMenuItem = "inputFrameRateMenuItem";
+	public const string MovieFrameRateMenuItem = "movieFrameRateMenuItem";
+	public const string AdjustTimingsMenuItem = "adjustTimingsMenuItem";
+	public const string ShiftMenuItem = "shiftTimingsMenuItem";
+	
+	/* Toolbar */
+	public const string Toolbar = "toolbar";
+	public const string SaveButton = "toolbuttonSave";
+	public const string UndoButton = "toolbuttonUndo";
+	public const string RedoButton = "toolbuttonRedo";
+	public const string CutButton = "toolbuttonCut";
+	public const string CopyButton = "toolbuttonCopy";
+	public const string PasteButton = "toolbuttonPaste";
+	public const string FindButton = "toolbuttonFind";
+	public const string ReplaceButton = "toolbuttonReplace";
+	public const string BoldButton = "toolbuttonBold";
+	public const string ItalicButton = "toolbuttonItalic";
+	public const string UnderlineButton = "toolbuttonUnderline";
+	
+	
+	/* Subtitle View */
+	
+	public const string SubtitleView = "subtitleListView";
+	
+	/* Subtitle Edit */
+	public const string SubtitleEditHBox = "editAreaHBox";
+	public const string StartSpinButton = "startSpinButton";
+	public const string EndSpinButton = "endSpinButton";
+	public const string DurationSpinButton = "durationSpinButton";
+	public const string SubtitleTextView = "subtitleTextView";
+
+	
+}
+
+}
---
+++ src/GnomeSubtitles/Dialogs/AdjustTimingsDialog.cs
@@ -0,0 +1,160 @@
+/*
+ * This file is part of Gnome Subtitles, a subtitle editor for Gnome.
+ * Copyright (C) 2006 Pedro Castro
+ *
+ * Gnome Subtitles is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Gnome Subtitles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+using System;
+using Glade;
+using Gtk;
+using SubLib;
+
+namespace GnomeSubtitles {
+
+
+public class AdjustTimingsDialog : GladeDialog {
+	private TimingMode timingMode;
+	
+	/* Constant strings */
+	private const string dialogName = "adjustTimingsDialog";
+	
+	/* Widgets */
+	
+	[WidgetAttribute]
+	private Label firstSubtitleStartLabel;
+	[WidgetAttribute]
+	private Label firstSubtitleNoInputLabel;
+	[WidgetAttribute]
+	private Label firstSubtitleStartInputLabel;
+	[WidgetAttribute]
+	private SpinButton firstSubtitleNewStartSpinButton;
+	[WidgetAttribute]
+	private Label lastSubtitleStartLabel;
+	[WidgetAttribute]
+	private Label lastSubtitleNoInputLabel;
+	[WidgetAttribute]
+	private Label lastSubtitleStartInputLabel;
+	[WidgetAttribute]
+	private SpinButton lastSubtitleNewStartSpinButton;
+	[WidgetAttribute]
+	private RadioButton allSubtitlesRadioButton;	
+	[WidgetAttribute]
+	private RadioButton selectedSubtitlesRadioButton;
+	
+
+	public AdjustTimingsDialog () : base(dialogName){
+		timingMode = Global.TimingMode; 
+		SetSpinButtons();
+		UpdateForTimingMode();
+		SetApplyToSelectionSensitivity();
+		SetApplyToAll();
+	}
+	
+	private void SetSpinButtons () {
+		int width = Util.SpinButtonTimeWidth(firstSubtitleNewStartSpinButton);
+		firstSubtitleNewStartSpinButton.WidthRequest = width;
+		lastSubtitleNewStartSpinButton.WidthRequest = width;
+	}
+
+	private void UpdateForTimingMode () {
+		if (timingMode == TimingMode.Times) {
+			string startLabel = "Start Time:";
+			firstSubtitleStartLabel.Text = startLabel;
+			lastSubtitleStartLabel.Text = startLabel;
+		}
+		Util.SetSpinButtonTimingMode(firstSubtitleNewStartSpinButton, timingMode, false);
+		Util.SetSpinButtonTimingMode(lastSubtitleNewStartSpinButton, timingMode, false);
+	}	
+
+	private void SetApplyToAll () {
+		SubtitleCollection collection = Global.Subtitles.Collection;
+		
+		int firstNo = 1;
+		int lastNo = collection.Count;		
+		UpdateInputValues(firstNo, lastNo);
+	}
+	
+	private void SetApplyToSelection () {
+		TreePath firstPath = Global.GUI.View.Selection.FirstPath;
+		TreePath lastPath = Global.GUI.View.Selection.LastPath;
+		
+		int firstNo = firstPath.Indices[0] + 1;
+		int lastNo = lastPath.Indices[0] + 1;
+		
+		UpdateInputValues (firstNo, lastNo);
+	}
+	
+	private void SetApplyToSelectionSensitivity () {
+		int selectionCount = Global.GUI.View.Selection.Count;
+		if (selectionCount < 2)
+			selectedSubtitlesRadioButton.Sensitive = false;
+	}
+
+	private void UpdateInputValues (int firstNo, int lastNo) {
+		SubtitleCollection collection = Global.Subtitles.Collection;
+		Subtitle firstSubtitle = collection.Get(firstNo - 1);
+		Subtitle lastSubtitle = collection.Get(lastNo - 1);
+	
+		firstSubtitleNoInputLabel.Text = firstNo.ToString();
+		lastSubtitleNoInputLabel.Text = lastNo.ToString();
+		
+		if (timingMode == TimingMode.Frames) {
+			firstSubtitleStartInputLabel.Text = firstSubtitle.Frames.Start.ToString();
+			firstSubtitleNewStartSpinButton.Value = firstSubtitle.Frames.Start;
+			lastSubtitleStartInputLabel.Text = lastSubtitle.Frames.Start.ToString();
+			lastSubtitleNewStartSpinButton.Value = lastSubtitle.Frames.Start;
+		}
+		else {
+			firstSubtitleStartInputLabel.Text = Util.TimeSpanToText(firstSubtitle.Times.Start);
+			firstSubtitleNewStartSpinButton.Value = firstSubtitle.Times.Start.TotalMilliseconds;
+			lastSubtitleStartInputLabel.Text = Util.TimeSpanToText(lastSubtitle.Times.Start);
+			lastSubtitleNewStartSpinButton.Value = lastSubtitle.Times.Start.TotalMilliseconds;
+		}
+	}
+
+	#pragma warning disable 169		//Disables warning about handlers not being used
+	
+	private void OnToggleAllSubtitles (object o, EventArgs args) {
+		if ((o as RadioButton).Active)
+			SetApplyToAll();
+	}
+	
+	private void OnToggleSelectedSubtitles (object o, EventArgs args) {
+		if ((o as RadioButton).Active)
+			SetApplyToSelection();
+	}
+	
+	private void OnResponse (object o, ResponseArgs args) {
+		if (args.ResponseId == ResponseType.Ok) {
+			SelectionType selectionType = (allSubtitlesRadioButton.Active ? SelectionType.All : SelectionType.Range);
+			
+			if (timingMode == TimingMode.Times) {
+				TimeSpan firstTime = TimeSpan.Parse(firstSubtitleNewStartSpinButton.Text);
+				TimeSpan lastTime = TimeSpan.Parse(lastSubtitleNewStartSpinButton.Text);
+				Global.CommandManager.Execute(new AdjustTimingsCommand(firstTime, lastTime, selectionType));
+			}
+			else {
+				int firstFrame = (int)firstSubtitleNewStartSpinButton.Value;
+				int lastFrame = (int)lastSubtitleNewStartSpinButton.Value;
+				Global.CommandManager.Execute(new AdjustTimingsCommand(firstFrame, lastFrame, selectionType));
+			}
+		}
+		CloseDialog();
+	}
+
+}
+
+}
