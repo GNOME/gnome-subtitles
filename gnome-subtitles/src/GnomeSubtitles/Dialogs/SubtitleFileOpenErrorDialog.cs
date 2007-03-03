8ecfa95411ba20026e9dd96e777935ffa3a150a3
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-03-03 16:32:14 +0000

    Part of the old FileOpenErrorDialog class. It contains the specific parts to subtitle file opening.
    svn path=/trunk/; revision=495

---
+++ gnome-subtitles/src/GnomeSubtitles/Dialogs/SubtitleFileOpenErrorDialog.cs
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Gnome Subtitles, a subtitle editor for Gnome.
+ * Copyright (C) 2006-2007 Pedro Castro
+ *
+ * Gnome Subtitles is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Gnome Subtitles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+using SubLib;
+using System;
+using System.IO;
+
+namespace GnomeSubtitles {
+
+public class SubtitleFileOpenErrorDialog : FileOpenErrorDialog {
+
+	public SubtitleFileOpenErrorDialog (string filename, Exception exception) : base(filename, exception) {
+	}
+
+	/* Overriden members */
+	
+	protected override string SecondaryTextFromException (Exception exception) {
+		if (exception is UnknownSubtitleFormatException)
+			return "Unable to detect the subtitle format. Please check that the file type is supported.";
+		else if (exception is OutOfMemoryException)
+			return "You have run out of memory. Please close some programs and try again.";
+		else if (exception is IOException)
+			return "An I/O error has occured.";
+		else
+			return GetGeneralExceptionErrorMessage(exception);
+	}
+
+}
+
+}
