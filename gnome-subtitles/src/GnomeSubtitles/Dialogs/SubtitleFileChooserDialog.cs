f48731b2f604eb58829c166be94f10838bb14ba8
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-03-24 00:34:49 +0000

    Store EncodingDescriptions which are automatically updated based on GConf values, instead of the old static encodings' list that was made available before. Setting the last item in the TreeModel will open the Encoding selection dialog.
    svn path=/trunk/; revision=572

--- gnome-subtitles/src/GnomeSubtitles/Dialogs/SubtitleFileChooserDialog.cs
+++ gnome-subtitles/src/GnomeSubtitles/Dialogs/SubtitleFileChooserDialog.cs
@@ -19,6 +19,7 @@
 
 using Gtk;
 using System;
+using System.Collections;
 using System.Text;
 
 namespace GnomeSubtitles {
@@ -26,44 +27,110 @@ namespace GnomeSubtitles {
 public class SubtitleFileChooserDialog : GladeDialog {
 
 	/* Protected variables */
-	protected string chosenFilename = String.Empty;
-	protected Encoding chosenEncoding = null;
 	protected new FileChooserDialog dialog = null;
-	protected FileEncoding[] encodings = {
-		new FileEncoding(65001, "Unicode", "UTF-8"),
-		new FileEncoding(28605, "Western", "ISO-8859-15"),
-		new FileEncoding("Current Locale")
-	};
+	
+	protected string chosenFilename = String.Empty;
+	protected EncodingDescription chosenEncoding;
+	protected bool hasChosenEncoding = false;
+	
+	protected EncodingDescription[] encodings = null;
 
 	public SubtitleFileChooserDialog (string filename) : base(filename) {
 		dialog = base.dialog as FileChooserDialog;
 	}
 	
+	/* Public properties */
+	
 	public string Filename {
 		get { return chosenFilename; }
 	}
 	
-	public Encoding Encoding {
+	public EncodingDescription ChosenEncoding {
 		get { return chosenEncoding; }
 	}
 	
-	public bool HasEncoding {
-		get { return chosenEncoding != null; }
+	public bool HasChosenEncoding {
+		get { return hasChosenEncoding; }
 	}
 	
 	/* Protected members */
-	
-	protected void FillEncodingComboBox (ComboBox comboBox) {
-		foreach (FileEncoding encoding in encodings)
-			comboBox.AppendText(encoding.Description + " (" + encoding.Name + ")");
 
-		comboBox.RowSeparatorFunc = SeparatorFunc;		
-		comboBox.Active = 0;
+	protected void SetEncodingComboBox (ComboBox comboBox) {
+		comboBox.RowSeparatorFunc = SeparatorFunc;
+		FillEncodingComboBox(comboBox, Global.Config.PrefsEncodingsShownInMenu);
 	}
 		
 	protected bool SeparatorFunc (TreeModel model, TreeIter iter) {
 		string text = (string)model.GetValue(iter, 0);
 		return ((text != null) && (text.CompareTo("-") == 0));
+	}
+	
+	/* Private members */
+	
+	private void LoadEncodings (string[] names) {
+		ArrayList encodings = new ArrayList();
+		
+		foreach (string name in names) {
+			EncodingDescription description = new EncodingDescription();
+			if (Encodings.Find(name, ref description))
+				encodings.Add(description);
+		}
+		
+		encodings.Sort();
+		encodings.Insert(0, Encodings.SystemDefault);
+		
+		this.encodings = (EncodingDescription[])encodings.ToArray(typeof(EncodingDescription));	
+	}
+	
+	private void FillEncodingComboBox (ComboBox comboBox, string[] names) {
+		LoadEncodings(names);
+		
+		foreach (EncodingDescription encoding in encodings)
+			comboBox.AppendText(encoding.Description + " (" + encoding.Name + ")");
+	
+		comboBox.AppendText("-");
+		comboBox.AppendText("Add or Remove...");
+		
+		comboBox.Active = 0;
+	}
+	
+	private void UpdateEncodingComboBox (ComboBox comboBox, string[] names) {
+	
+		/* Get the first elements that are not in the model */
+		int itemCount = comboBox.Model.IterNChildren();
+		int knownCount = encodings.Length + 2; //Add 2 for horizontal bar and item at the end
+		int remainingCount = itemCount - knownCount;
+		
+		string[] remainingItems = new string[remainingCount];
+		int rowNumber = 0;
+		ListStore store = comboBox.Model as ListStore;
+		foreach (object[] row in store) {
+			if (rowNumber == remainingCount)
+				break;
+			
+			remainingItems[rowNumber] = row[0] as string;
+			rowNumber++;			
+		}
+		
+		store.Clear();
+		
+		foreach (string item in remainingItems)
+			comboBox.AppendText(item);
+		
+		FillEncodingComboBox(comboBox, names);
+	}
+	
+	#pragma warning disable 169		//Disables warning about handlers not being used
+	
+	private void OnEncodingComboBoxChanged (object o, EventArgs args) {
+		ComboBox comboBox = o as ComboBox;
+		int itemCount = comboBox.Model.IterNChildren();
+		if (comboBox.Active == (itemCount - 1)) {
+			EncodingsDialog dialog = new EncodingsDialog();
+			dialog.WaitForResponse();
+			UpdateEncodingComboBox(comboBox, dialog.ChosenNames);
+		}
+		
 	}
 
 }
