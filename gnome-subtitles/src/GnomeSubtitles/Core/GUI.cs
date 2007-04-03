1cfeb837cec8a3e82b8935d122ce597b76a8bee8
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-04-03 22:46:35 +0000

    Open a matching video when running GS with a filename as argument (if the option is set in the preferences) Updated to reflect the use of the new Document class, which contains all the functionality for creating Documents/Subtitles. Methods that propagate events throughout the GUI now have the UpdateFrom prefix. Code cleanup. TimingModes are now set in the Menus directly.
    svn path=/trunk/; revision=607

--- gnome-subtitles/src/GnomeSubtitles/Core/GUI.cs
+++ gnome-subtitles/src/GnomeSubtitles/Core/GUI.cs
@@ -49,6 +49,7 @@ public class GUI {
 		menus = new Menus();
 
 		glade.Autoconnect(handlers); //TODO think about using separate connections for different parts of the GUI
+		
 		window.Visible = true;
     }
 
@@ -80,8 +81,11 @@ public class GUI {
     /// <summary>Starts the GUI</summary>
     /// <remarks>A file is opened if it was specified as argument. If it wasn't, a blank start is performed.</summary>
     public void Start () {
-    	if (ExecutionInfo.Args.Length > 0)
-			Open(ExecutionInfo.Args[0], null, String.Empty);
+    	if (ExecutionInfo.Args.Length > 0) {
+    		string subtitleFile = ExecutionInfo.Args[0];
+    		string videoFile = Global.Config.PrefsVideoAutoChooseFile ? VideoFiles.FindMatchingVideo(subtitleFile) : String.Empty;
+			Open(ExecutionInfo.Args[0], null, videoFile);
+		}
 		else
 			BlankStartUp();
     }
@@ -99,25 +103,22 @@ public class GUI {
 		window.Destroy();
     }
 
-	/// <summary>Creates a new subtitles document, corresponding it to the specified filename.</summary>
-	/// <param name="filename">The subtitles' filename. If it's an empty string, 'Unsaved Subtitles' will be used instead.</param>
-	/// <remarks>If there's a document currently opened with unsaved changes, a warning dialog is shown.</remarks>
-    public void New (string filename) {
+	/// <summary>Creates a new subtitles document for the specified path.</summary>
+	/// <param name="path">The subtitles' filename. If it's an empty string, 'Unsaved Subtitles' will be used instead.</param>
+	/// <remarks>If there's a document opened with unsaved changes, a warning dialog is shown.</remarks>
+    public void New (string path) {
     	if (!ToCreateNewAfterWarning)
     		return;
 
-		if (filename == String.Empty)
-			filename = "Unsaved Subtitles";
+		if (path == String.Empty)
+			path = "Unsaved Subtitles";
 
-   		SubtitleFactory factory = new SubtitleFactory();
-		factory.Verbose = true;
-		Subtitles subtitles = new Subtitles(factory.NewWithName(filename));
-		
-   		NewDocument(subtitles, TimingMode.Times);
+		Document document = new Document(path);
+		Global.Document = document;
 
-		if (subtitles.Count == 0) {
+		if (document.Subtitles.Count == 0) {
 			Global.CommandManager.Execute(new InsertFirstSubtitleCommand());
-			Global.GUI.View.Selection.ActivatePath();
+			Global.GUI.View.Selection.ActivatePath(); //TODO is this necessary?
 		}
     }
     
@@ -158,8 +159,8 @@ public class GUI {
     /// <remarks>If the document hasn't been saved before, a SaveAs is executed.</remarks>
     /// <returns>Whether the file was saved or not.</returns>
     public bool Save () {
-    	if (Global.Subtitles.CanSave) { //Check if document can be saved or needs a SaveAs
-			Global.Subtitles.Save();
+    	if (Global.Document.CanBeSaved) { //Check if document can be saved or needs a SaveAs
+			Global.Document.Save();
 			Global.CommandManager.WasModified = false;
 			UpdateWindowTitle(false);
 			return true;
@@ -167,7 +168,7 @@ public class GUI {
 		else
 			return SaveAs();
     }
-    
+
     /// <summary>Executes a SaveAs operation.</summary>
     /// <remarks>After saving, the timing mode is set to the timing mode of the subtitle format using when saving.</remarks>
     /// <returns>Whether the file was saved or not.</returns>
@@ -178,22 +179,35 @@ public class GUI {
 			string filename = dialog.Filename;
 			SubtitleType type = dialog.SubtitleType;
 			Encoding encoding = Encoding.GetEncoding(dialog.ChosenEncoding.CodePage);
-			Global.Subtitles.SaveAs(filename, type, encoding);
-			TimingMode newTimingMode = Global.Subtitles.Properties.TimingMode; 
-			Global.TimingMode = newTimingMode;
+
+			Global.Document.Save(filename, encoding, type);
 			Global.CommandManager.WasModified = false;
 			UpdateWindowTitle(false);
-			OnToggleTimingMode(newTimingMode);
 			return true;
 		}
 		else
 			return false;
 	}
-	
+
 	public void UpdateWindowTitle (bool modified) {
 		string prefix = (modified ? "*" : String.Empty);
-		window.Title = prefix + Global.Subtitles.Properties.FileName +
+		window.Title = prefix + Global.Document.FileProperties.Filename +
 			" - " + ExecutionInfo.ApplicationName;
+	}
+	
+	public void UpdateFromNewDocument (bool wasLoaded) {
+	   	Global.CommandManager.Clear();
+
+		UpdateWindowTitle(false);
+		view.UpdateFromNewDocument(wasLoaded);
+		edit.UpdateFromNewDocument(wasLoaded);
+		menus.UpdateFromNewDocument(wasLoaded);
+	}
+	
+	public void UpdateFromTimingMode (TimingMode mode) {
+		view.UpdateFromTimingMode(mode);
+		edit.UpdateFromTimingMode(mode);
+		video.UpdateFromTimingMode(mode);
 	}
 	
 	/// <summary>Updates the various parts of the GUI based on the current selection.</summary>
@@ -206,7 +220,7 @@ public class GUI {
 	
 	/// <summary>Updates the various parts of the GUI based on the current subtitle count.</summary>
 	public void UpdateFromSubtitleCount () {
-		int count = Global.Subtitles.Collection.Count;
+		int count = Global.Document.Subtitles.Collection.Count;
 		menus.UpdateFromSubtitleCount(count);
 	}
 
@@ -214,42 +228,28 @@ public class GUI {
 	/* Private members */
 	
 	/// <summary>Opens a subtitle file, given its filename and encoding.</summary>
-	/// <param name="filename">The filename to open.</param>
+	/// <param name="path">The path of the subtitles file to open.</param>
 	/// <param name="encoding">The encoding of the filename. To use autodetection, set it to null.</param>
 	/// <param name="videoFilename">The videoFilename to open. If <see cref="String.Empty" />, no video will be opened.</param>
-    private void Open (string filename, Encoding encoding, string videoFilename) {
-		SubtitleFactory factory = new SubtitleFactory();
-		factory.Verbose = true;
-		if (encoding != null)
-			factory.Encoding = encoding;
+    private void Open (string path, Encoding encoding, string videoFilename) {
+    	Global.Document = new Document(path, encoding);
+    	Global.Document.UpdateTimingModeFromFileProperties();
 
-		SubLib.Subtitles openedSubtitles = null;
-		try {
-			openedSubtitles = factory.Open(filename);
-		}
-		catch (FileNotFoundException) {
-			New(filename);
-			return;
-		}
-		Subtitles subtitles = new Subtitles(openedSubtitles);
-		TimingMode timingMode = subtitles.Properties.TimingMode;
-
-    	NewDocument(subtitles, timingMode);
-		view.Selection.SelectFirst();
+		view.Selection.SelectFirst(); //TODO is this needed?
 		
 		if (videoFilename != String.Empty)
 			OpenVideo(videoFilename);
     }
     
-    private void OpenVideo (string filename) {
+    private void OpenVideo (string path) {
     	Menus.SetViewVideoActivity(true);
 		try {
-			Video.Open(filename);
+			Video.Open(path);
 			Menus.SetVideoSensitivity(true);
 		}
 		catch (Exception exception) {
 			Video.Close();
-			VideoFileOpenErrorDialog errorDialog = new VideoFileOpenErrorDialog(filename, exception);
+			VideoFileOpenErrorDialog errorDialog = new VideoFileOpenErrorDialog(path, exception);
 			bool toOpenAgain = errorDialog.WaitForResponse();
 	    	if (toOpenAgain)
 	    		OpenVideo(); //Recursive call to open the dialog again
@@ -263,21 +263,6 @@ public class GUI {
     	view.BlankStartUp();
     	edit.BlankStartUp();
     }
-     
-    /// <summary>Sets the window for a new document.</summary>
-    /// <remarks>A new document can be the result of Open or New operations.</remarks>
-	private void NewDocument (Subtitles subtitles, TimingMode mode) {
-   		bool wasLoaded = Global.AreSubtitlesLoaded;
-   		
-   		Global.CommandManager.Clear();
-   		Global.Subtitles = subtitles;
-   		Global.TimingMode = mode;
-
-		UpdateWindowTitle(false);
-		menus.NewDocument(wasLoaded);
-		view.NewDocument(wasLoaded);
-		edit.NewDocument(wasLoaded);
-	}
 		
 	/// <summary>Updates the GUI from the specified selected Subtitle.</summary>
 	/// <param name="subtitle">The subtitle that is currently selected.</param>
@@ -298,7 +283,7 @@ public class GUI {
 		edit.Enabled = false;
 		video.UpdateFromSelection(false);
 	}    
-	
+
 	/* Private properties */
 	
 	/// <summary>Whether there are unsaved changes.</summary>
@@ -331,7 +316,7 @@ public class GUI {
 	}
 	
 	/// <summary>Whether a document should be opened, after choosing the respective confirmation dialog.</summary>
-	private bool ToOpenAfterWarning {
+	private bool ToOpenAfterWarning { //TODO turn this into a function
     	get {
     		if (ExistUnsavedChanges) {
 	    		SaveConfirmationDialog dialog = new SaveOnOpenConfirmationDialog();
@@ -340,16 +325,6 @@ public class GUI {
     		else
 	    		return true; 
 		}
-	}
-	
-	
-	/* Event members */
-	
-	public void OnToggleTimingMode (TimingMode newMode) {
-		Global.TimingMode = newMode;
-		view.ToggleTimingMode(newMode);
-		edit.ToggleTimingMode(newMode);
-		video.ToggleTimingMode(newMode);
 	}
 
 }
