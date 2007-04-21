baba082eb9ca31faf832475091b1fbc072e01190
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-04-21 15:52:03 +0000

    Updated to the gnome-common autogen.sh.
    svn path=/trunk/; revision=684

--- gnome-subtitles/autogen.sh
+++ gnome-subtitles/autogen.sh
@@ -1,73 +1,18 @@
-#! /bin/sh
-
-PROJECT=gnome-subtitles
-CONFIGURE=configure.in
-
-: ${AUTOCONF=autoconf}
-: ${AUTOHEADER=autoheader}
-: ${AUTOMAKE=automake}
-: ${LIBTOOLIZE=libtoolize}
-: ${ACLOCAL=aclocal}
-: ${LIBTOOL=libtool}
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-ORIGDIR=`pwd`
-cd $srcdir
-TEST_TYPE=-f
-aclocalinclude="-I . $ACLOCAL_FLAGS"
+REQUIRED_AUTOMAKE_VERSION=1.9
 
-DIE=0
+PKG_NAME="gnome-subtitles"
 
-($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have autoconf installed to compile $PROJECT."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/README) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have automake installed to compile $PROJECT."
-        echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.9.tar.gz"
-        echo "(or a newer version if it is available)"
-        DIE=1
-}
-
-(grep "^AM_PROG_LIBTOOL" $CONFIGURE >/dev/null) && {
-  ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "**Error**: You must have \`libtool' installed to compile $PROJECT."
-    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
-  }
-}
-
-if test "$DIE" -eq 1; then
-        exit 1
-fi
-                                                                                
-case $CC in
-*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
-esac
-
-(grep "^AM_PROG_LIBTOOL" $CONFIGURE >/dev/null) && {
-    echo "Running $LIBTOOLIZE ..."
-    $LIBTOOLIZE --force --copy
-}
-
-echo "Running $ACLOCAL $aclocalinclude ..."
-$ACLOCAL $aclocalinclude
-
-echo "Running $AUTOMAKE $am_opt ..."
-$AUTOMAKE --add-missing --copy $am_opt
-
-echo "Running $AUTOCONF ..."
-$AUTOCONF
-
-echo Running $srcdir/configure $conf_flags "$@" ...
-$srcdir/configure $conf_flags "$@" \
-
+. $srcdir/macros2/gnome-autogen.sh
