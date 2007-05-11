76d4f8c2f7760103c65c7772bbb3a3873934a757
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-05-11 18:44:17 +0000

    Changed the autotools scripts to use gnome-common, and to build/install help and translation files.
    svn path=/trunk/; revision=717

--- gnome-subtitles/autogen.sh
+++ gnome-subtitles/autogen.sh
@@ -4,15 +4,23 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-REQUIRED_AUTOMAKE_VERSION=1.9
-
 PKG_NAME="gnome-subtitles"
 
-(test -f $srcdir/configure.in \
-  && test -f $srcdir/README) || {
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/README \
+  && test -d $srcdir/src/GnomeSubtitles) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-. $srcdir/macros2/gnome-autogen.sh
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from GNOME SVN and make"
+    echo "sure the gnome-autogen.sh script is in your \$PATH."
+    exit 1
+}
+
+REQUIRED_AUTOMAKE_VERSION=1.9
+USE_GNOME2_MACROS=1
+
+. gnome-autogen.sh
