91ffbf166f37648955f561fa5f65d564d22a155e
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-06-21 20:44:06 +0000

    Improved comments.
    svn path=/trunk/; revision=807

--- src/GnomeSubtitles/Commands/FixedMultipleSelectionCommand.cs
+++ src/GnomeSubtitles/Commands/FixedMultipleSelectionCommand.cs
@@ -27,7 +27,7 @@ public abstract class FixedMultipleSelectionCommand : MultipleSelectionCommand {
 	
 	/// <summary>Creates a new instance of the <see cref="MultipleFixedSelectionCommand" /> class.</summary>
 	/// <param name="description">The description of the command.</param>
-	/// <param name="canGroup">Whether to group the command with similar commands.</param>
+	/// <param name="canGroup">Whether to possibly group the command with the previous command.</param>
 	/// <param name="selectionIntended">The intended selection.</param>
 	/// <param name="reselect">Whether to reselect the command when executing. Note that this doesn't apply to Undo nor to Redo.</param>
 	public FixedMultipleSelectionCommand (string description, bool canGroup, SelectionIntended selectionIntended, bool reselect) : base(description, canGroup, selectionIntended) {
