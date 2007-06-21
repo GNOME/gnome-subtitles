6ed04e99908367afe3566a42a1cbbacfb899cb71
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-06-21 20:42:42 +0000

    New commands with improved text deletion. The selection is stored in order to clearly mark it (selecting it) when undoing. Grouping is used to group characters that were deleted in sequence. Grouping stops on space characters.
    svn path=/trunk/; revision=805

---
+++ src/GnomeSubtitles/Commands/DeleteTranslationCommand.cs
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Gnome Subtitles.
+ * Copyright (C) 2007 Pedro Castro
+ *
+ * Gnome Subtitles is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Gnome Subtitles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+using Mono.Unix;
+using SubLib;
+
+namespace GnomeSubtitles {
+
+public class DeleteTranslationCommand : DeleteTextContentCommand {
+	private static string description = Catalog.GetString("Editing Translation"); //Must be equal to InsertTranslationCommand
+
+	public DeleteTranslationCommand (int index, string text, int cursor) : base(description, index, text, cursor) {
+		SetCommandTarget(CommandTarget.Translation);
+	}
+	
+	protected override SubtitleEditTextView GetTextView () {
+		return Global.GUI.Edit.TranslationEdit;
+	}
+
+}
+
+}
