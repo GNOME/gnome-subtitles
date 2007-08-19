cf1fbd668f3c31f1cd9389c9b74f8abadb0cea85
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2007-08-19 04:05:08 +0000

    Dialog to report errors found during video playback.
    svn path=/trunk/; revision=885

---
+++ src/GnomeSubtitles/Dialogs/VideoErrorDialog.cs
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Gnome Subtitles.
+ * Copyright (C) 2007 Pedro Castro
+ *
+ * Gnome Subtitles is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Gnome Subtitles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+using Gtk;
+using Mono.Unix;
+using System;
+
+namespace GnomeSubtitles {
+
+public class VideoErrorDialog : ErrorDialog {
+	private static string primary = Catalog.GetString("Could not continue the video playback");
+	private static string secondaryInitial = Catalog.GetString("The following error has occurred: ");
+	
+	public VideoErrorDialog (string secondary) : base(primary, secondaryInitial + secondary) {
+	}
+
+	
+	/* Protected methods */
+	
+	protected override void AddButtons () {
+		dialog.AddButton(Stock.Ok, ResponseType.Ok);
+	}
+	
+
+}
+
+}
