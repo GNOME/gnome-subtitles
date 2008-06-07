1b2b9598a150b05aa46733d9ec3905be3110aec1
Pedro Daniel da Rocha Melo e Castro <pcastro@src.gnome.org>
2008-06-07 21:20:13 +0000

    Increased undo limit.
    svn path=/trunk/; revision=991

--- src/GnomeSubtitles/Commands/CommandManager.cs
+++ src/GnomeSubtitles/Commands/CommandManager.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of Gnome Subtitles.
- * Copyright (C) 2006-2007 Pedro Castro
+ * Copyright (C) 2006-2008 Pedro Castro
  *
  * Gnome Subtitles is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ using System;
 namespace GnomeSubtitles {
 
 public class CommandManager {
-	private int limit = 25;
+	private int limit = 40;
 	private Command[] commands = null;
 
 	private int undoCount = 0;
