e926263501bcd64f3b2919775d10f19f32ddd5e9
Pedro Castro <mail@pedrocastro.org>
2010-12-19 16:48:28 +0000

    Fix copyright header.

--- src/GnomeSubtitles/Dialog/FileTranslationOpenDialog.cs
+++ src/GnomeSubtitles/Dialog/FileTranslationOpenDialog.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of Gnome Subtitles.
- * Copyright (C) 2007-2008 Pedro Castro
+ * Copyright (C) 2007-2010 Pedro Castro
  *
  * Gnome Subtitles is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
