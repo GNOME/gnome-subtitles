6471c147e3942c2c0975a5d44a54d4d188e86306
Pedro Castro <mail@pedrocastro.org>
2011-06-12 23:29:44 +0100

    Code cleanup

--- src/GnomeSubtitles/Dialog/TimingsShiftDialog.cs
+++ src/GnomeSubtitles/Dialog/TimingsShiftDialog.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of Gnome Subtitles.
- * Copyright (C) 2006-2010 Pedro Castro
+ * Copyright (C) 2006-2011 Pedro Castro
  *
  * Gnome Subtitles is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@ using System;
 namespace GnomeSubtitles.Dialog {
 
 //TODO set spinButton limits according to selection type
-//FIXME show informative message about the Ctrl+Shift++ shortcuts
+//TODO show informative message about the Ctrl+Shift++ shortcuts
 public class TimingsShiftDialog : GladeDialog {
 	private TimingMode timingMode = TimingMode.Frames;
 
